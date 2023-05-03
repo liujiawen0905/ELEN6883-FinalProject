66e4dfb18910ed82b2bf08ae554e8506b4712a75
Jiawen Liu <liujiawen@jiawens-mbp.mynetworksettings.com>
2023-05-03 15:57:30 -0400

    migrate

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,9 @@
+const NFT = artifacts.require("NFT");
 const NFTMarketplace = artifacts.require("NFTMarketplace");
 
+
 module.exports = function(deployer) {
-  deployer.deploy(NFTMarketplace);
-  deployer.link(NFTMarketplace);
-  deployer.deploy(NFTMarketplace);
+  deployer.deploy(NFT);
+  deployer.link(NFT, NFTMarketplace);
+  deployer.deploy(NFTMarketplace, "0x81471980D76Fe40b43835A887Fb43791484f42b8");
 };
