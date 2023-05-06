db46d0c67e82600a649777295b9fdf48e25552a8
Haohsin Hsu <haohsinhsu@Haohsins-MacBook-Air.local>
2023-05-06 16:58:10 -0400

    add testNFTCreate function

--- test/TestNFTMarketplace.sol
+++ test/TestNFTMarketplace.sol
@@ -8,6 +8,27 @@ import "../contracts/NFTMarketplace.sol";
 
 contract TestNFTMarketplace {
 
+  function testCreateNFT() public {
+
+    // Initialize NFT market
+    NFTMarketplace market = new NFTMarketplace(0x81471980D76Fe40b43835A887Fb43791484f42b8);
+
+    // Create NFT
+    uint NFT_id = 1;
+    string NFT_name = "Name";
+    string NFT_desc = "Desc";
+    market.createNFT(NFT_id, NFT_name, NFT_desc);
+
+    // Get NFT
+    Token storage NFT = market.idToToken[NFT_id];
+
+    // Assertion
+    assert.equal(Token.unique_id, NFT_id, "Incorrect NFT ID");
+    assert.equal(Token.name, NFT_name, "Incorrect NFT name");
+    assert.equal(Token.description, NFT_desc, "Incorrect NFT description");
+    assert.equal(Token.owner, accounts[0], "Incorrect NFT owner");
+  }
+
   function testInitialBalanceWithNewMetaCoin() public {
     NFTMarketplace market = new NFTMarketplace(0x81471980D76Fe40b43835A887Fb43791484f42b8);
 
