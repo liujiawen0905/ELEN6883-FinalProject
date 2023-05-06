9fc2cb43560d417f8fa83d0b5deec2969a213bee
Jiawen Liu <liujiawen@Jiawens-MacBook-Pro.local>
2023-05-06 19:09:45 -0400

    update contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -3,162 +3,123 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./NFT.sol";
 
-contract NFTMarketplace {
-
-    address payable private owner;
-
+contract NFTMarketplace is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _marketTokenCounts;
 
-    uint256 tokenPrice = 0.1 ether;
-
-    NFT myNFT;
-
-    // The structure to store info about a listed token in blockchain
-    struct Token {
-        uint token_idx;
-        uint unique_id;
-        address payable owner;
-        address payable seller;
+    // The structure to store info about a NFT in blockchain
+    struct NFT {
+        uint id;
+        address owner;
         string name;
         uint price;
         string description;
-        bool is_sold;
         bool is_listed;
-    } 
+    }
 
-    //the event emitted when a token is successfully created
-    event TokenCreated (
-        uint indexed token_idx,
-        uint unique_id,
-        address owner,
-        address seller,
-        string name,
-        string description,
-        bool is_sold,
-        bool is_listed
-    );
+    address payable private marketOwner;
+    uint private royalty_percentage;
 
-    event TokenListed (
-        uint indexed token_idx,
-        uint unique_id,
-        uint price,
-        bool is_listed
-    );
-
-    event ListedTokenRemoved (
-        uint indexed token_idx,
-        uint unique_id,
-        bool is_listed
-    );
-
-    event BuyToken (
-        uint indexed token_idx,
-        uint unique_id,
-        bool is_sold,
-        address newOwner
-    );
+    uint256 tokenPrice = 0.1 ether;
 
     //mapping from token id to token information
-    mapping(uint => Token) public idToToken;
+    mapping(uint => NFT) public idToToken;
 
     //mapping from a user address to user's fund
     mapping(address => uint) public userFunds;
 
-    constructor(address _NFTAddress) {
-        myNFT = NFT(_NFTAddress);
+    constructor(string memory name, string memory symbol, uint percentage) ERC721(name, symbol){
+        royalty_percentage = percentage;
+        marketOwner = payable(msg.sender);
     }
 
     function getTokenPrice() public view returns(uint) {
         return tokenPrice;
     }
 
-    // Create NFT
-    function createNFT(uint id, string memory name, string memory description) public payable returns (uint){
+    // mint a new NFT and returns the id
+    function mintNFT() public returns (uint) {
         // increment the tokenId counter, which is keep track of the number of mined NFTs
         _marketTokenCounts.increment();
-        uint newTokenCount = _marketTokenCounts.current();
+        uint newTokenId = _marketTokenCounts.current();
+        _safeMint(msg.sender, newTokenId);
+        idToToken[newTokenId] = NFT(
+            newTokenId, 
+            msg.sender, 
+            "NULL", 
+            0 , 
+            "NULL", 
+            false
+            );
+        return newTokenId;
+    }
 
-         // update the mapping of tokenId's to token details
-        idToToken[id] = Token(
-            newTokenCount,
+    // Create NFT
+    function createNFT(uint id, string memory name, string memory description) public payable returns (uint){
+        _marketTokenCounts.increment();
+        uint newTokenId = _marketTokenCounts.current();
+        idToToken[id] = NFT(
             id,
-            payable(address(this)),
             payable(msg.sender),
             name,
             0,
             description,
-            false,
             false
         );
-
-        emit TokenCreated(
-            newTokenCount,
-            id,
-            payable(address(this)),
-            payable(msg.sender),
-            name, 
-            description,
-            false,
-            false
-        );
-        
-        return newTokenCount;
+        return newTokenId;
     }
 
     // transfer NFT function
-    function transferNFT(uint id) public payable{
+    function transferNFT(uint id, address toAddress) public payable{
+        NFT storage item = idToToken[id];
+        require(item.id == id, "The NFT must exist");
+        require(ownerOf(id) == msg.sender, "Current user do not own this NFT");
         //update the details of the token
-        idToToken[id].is_sold = true;
         idToToken[id].is_listed = false;
-        idToToken[id].seller = payable(msg.sender);
-
+        idToToken[id].owner = toAddress;
         //Actually transfer the token to the new owner
-        myNFT.transferFrom(address(this), msg.sender, id);
+        safeTransferFrom(address(this), toAddress, id);
     }
 
     // list NFT for sale
     function listNFTForSale(uint id, uint price) public {
-        Token storage item = idToToken[id];
-        require(item.unique_id == id, "The NFT must exist");
+        NFT storage item = idToToken[id];
+        require(item.id == id, "The NFT must exist");
         require(price > 0, "The price must be positive");
         require(item.owner == msg.sender, "Only the onwer can list a NFT for sale");
         require(!item.is_listed, "The NFT is already listed");
-
         item.is_listed = true;
         item.price = price;
-
-        emit TokenListed (item.token_idx, id, price, true);
     }
 
     // remove NFT From sale
     function removeNFTFromSale(uint id) public {
-        Token storage item = idToToken[id];
-        require(item.unique_id == id, "The NFT must exist");
+        NFT storage item = idToToken[id];
+        require(item.id == id, "The NFT must exist");
         require(item.owner == msg.sender, "The NFT can only be removed by owner");
-        require(!item.is_sold, "A sold NFT cannot be removed");
         require(item.is_listed, "The NFT has not been listed yet");
-
         item.is_listed = false;
-        emit ListedTokenRemoved(item.token_idx, id, false);
     }
 
     function purchaseNFT(uint id) public payable {
-        Token storage item = idToToken[id];
-        require(item.unique_id == id, "The NFT must exist");
+        NFT storage item = idToToken[id];
+        require(item.is_listed, "The NFT needs to be listed to be purchased");
+        require(item.id == id, "The NFT must exist");
         require(item.owner != msg.sender, "A NFT can not be purchased by the owner");
-        require(!item.is_sold, "A sold NFT cannot be purchased");
-        require(item.is_listed, "The NFT needs to be listed");
         require(msg.value >= item.price, "The buyer does not have enough money");
 
-        transferNFT(id);
-        item.is_sold = true;
-        emit BuyToken(item.token_idx, id, true, msg.sender);
-    }
+        // transfer money to seller and market owner
+        address payable seller = payable(item.owner);
+        uint256 sale_amount = item.price;
+        uint256 royalty_fee = sale_amount * (royalty_percentage) / 100;
+        seller.transfer(sale_amount);
+        marketOwner.transfer(royalty_fee);
 
-    function claimFunds() public payable {
-        // todo
-    }    
+        //Actually transfer the NFT to the new owner
+        item.is_listed = false;
+        item.price = 0;
+        item.owner = msg.sender;
+        safeTransferFrom(seller, msg.sender, id);
+    }
 }
