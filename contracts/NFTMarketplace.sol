e97043c37ee2972d8acc94e762e6e68067498e24
Haohsin Hsu <haohsinhsu@Haohsins-Air.lan>
2023-05-07 12:20:45 -0400

    refine code

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -20,7 +20,7 @@ contract NFTMarketplace is ERC721, Ownable{
     }
 
     address payable private marketOwner;
-    uint private royalty_percentage;
+    uint private commision_percentage;
 
     //mapping from token id to token information
     mapping(uint => NFT) public idToToken;
@@ -29,7 +29,7 @@ contract NFTMarketplace is ERC721, Ownable{
     mapping(address => uint) public userFunds;
 
     constructor(string memory name, string memory symbol, uint percentage) ERC721(name, symbol){
-        royalty_percentage = percentage;
+        commision_percentage = percentage;
         marketOwner = payable(msg.sender);
     }
 
@@ -40,7 +40,7 @@ contract NFTMarketplace is ERC721, Ownable{
 
     // Get commision percentage
     function commissionPercentage() public view returns (uint) {
-        return royalty_percentage;
+        return commision_percentage;
     }
 
     // mint a new NFT and returns the id
@@ -61,13 +61,13 @@ contract NFTMarketplace is ERC721, Ownable{
     }
 
     // Create NFT
-    function createNFT(uint id, string memory name, string memory description, address user_address) public payable returns (uint){
+    function createNFT(uint id, string memory name, string memory description) public payable returns (uint){
         _marketTokenCounts.increment();
         uint newTokenId = _marketTokenCounts.current();
-        _safeMint(user_address, id);
+        _safeMint(msg.sender, id);
         idToToken[id] = NFT(
             id,
-            user_address,
+            msg.sender,
             name,
             0,
             description,
@@ -77,33 +77,34 @@ contract NFTMarketplace is ERC721, Ownable{
     }
 
     // transfer NFT function
-    function transferNFT(uint id, address fromAddress, address toAddress) public payable{
+    function transferNFT(uint id, address toAddress) public payable{
         NFT storage item = idToToken[id];
         require(item.id == id, "The NFT must exist");
-        require(ownerOf(id) == msg.sender, "Current user do not own this NFT");
+        require(item.owner == msg.sender, "Current user do not own this NFT");
         //update the details of the token
         idToToken[id].is_listed = false;
         idToToken[id].owner = toAddress;
         //Actually transfer the token to the new owner
-        safeTransferFrom(fromAddress, toAddress, id);
+        // setApprovalForAll(fromAddress, true);
+        safeTransferFrom(msg.sender, toAddress, id);
     }
 
     // list NFT for sale
-    function listNFTForSale(uint id, uint price, address user_address) public {
+    function listNFTForSale(uint id, uint price) public {
         NFT storage item = idToToken[id];
         require(item.id == id, "The NFT must exist");
         require(price > 0, "The price must be positive");
-        require(item.owner == user_address, "Only the onwer can list a NFT for sale");
+        require(item.owner == msg.sender, "Only the onwer can list a NFT for sale");
         require(!item.is_listed, "The NFT is already listed");
         item.is_listed = true;
         item.price = price;
     }
 
     // remove NFT From sale
-    function removeNFTFromSale(uint id, address user_address) public {
+    function removeNFTFromSale(uint id) public {
         NFT storage item = idToToken[id];
         require(item.id == id, "The NFT must exist");
-        require(item.owner == user_address, "The NFT can only be removed by owner");
+        require(item.owner == msg.sender, "The NFT can only be removed by owner");
         require(item.is_listed, "The NFT has not been listed yet");
         item.is_listed = false;
     }
@@ -112,20 +113,20 @@ contract NFTMarketplace is ERC721, Ownable{
         NFT storage item = idToToken[id];
         require(item.is_listed, "The NFT needs to be listed to be purchased");
         require(item.id == id, "The NFT must exist");
-        require(item.owner != msg.sender, "A NFT can not be purchased by the owner");
+        require(item.owner != msg.sender, "A NFT can not be purchased by it's owner");
         require(msg.value >= item.price, "The buyer does not have enough money");
 
-        // transfer money to seller and market owner
+        // Transfer money to the seller and the market owner
         address payable seller = payable(item.owner);
         uint256 sale_amount = item.price;
-        uint256 royalty_fee = sale_amount * (royalty_percentage) / 100;
-        seller.transfer(sale_amount);
-        marketOwner.transfer(royalty_fee);
+        uint256 commision_fee = sale_amount * (commision_percentage) / 100;
+        seller.transfer(sale_amount - commision_fee);
+        marketOwner.transfer(commision_fee);
 
-        //Actually transfer the NFT to the new owner
+        // Transfer the NFT to the new owner and remove it from the market
+        _transfer(seller, msg.sender, id);
+        item.owner = msg.sender;
         item.is_listed = false;
         item.price = 0;
-        item.owner = msg.sender;
-        safeTransferFrom(seller, msg.sender, id);
     }
 }
--- test/TestNFTMarketplace.js
+++ test/TestNFTMarketplace.js
@@ -9,7 +9,7 @@ contract('NFTMarketplace', (accounts) => {
 
   // Test create NFT
   it('Test create NFT', async () => {
-    // Get account addrress
+    // Get account address
     const user1 = accounts[1];
 
     // Declare NFT info variables
@@ -18,24 +18,22 @@ contract('NFTMarketplace', (accounts) => {
     const nft1_description = 'DESC1';
 
     // Create NFT
-    await nftMarketplace.createNFT(nft1_id, nft1_name, nft1_description, user1);
+    await nftMarketplace.createNFT(nft1_id, nft1_name, nft1_description, { from: user1 });
 
     // Get NFT info
-    // const nft1 = await nftMarketplace.getNFT(nft1_id);
     const nft1 = await nftMarketplace.getNFT(nft1_id);
 
     // // Assertions
     assert.equal(nft1.id, nft1_id, "Incorrect NFT id");
-    // assert.equal(nft1.name, nft1_name, "Incorrect NFT name");
-    // assert.equal(nft1.description, nft1_description, "Incorrect NFT description");
-    // assert.equal(nft1.owner, user1, "Incorrect NFT owner");
+    assert.equal(nft1.name, nft1_name, "Incorrect NFT name");
+    assert.equal(nft1.description, nft1_description, "Incorrect NFT description");
+    assert.equal(nft1.owner, user1, "Incorrect NFT owner");
   });
 
   // Test transfer NFT
   it("Test transfer NFT", async () => {
     // Get account address
     const user2 = accounts[2];
-    const user3 = accounts[3];
 
     // Declare NFT info variables
     const nft2_id = 2;
@@ -43,21 +41,18 @@ contract('NFTMarketplace', (accounts) => {
     const nft2_description = 'DESC2';
 
     // Create NFT
-    await nftMarketplace.createNFT(nft2_id, nft2_name, nft2_description, user2);
+    await nftMarketplace.createNFT(nft2_id, nft2_name, nft2_description);
 
-    // Transfer the NFT from user1 to user2
-    await nftMarketplace.transferNFT(nft2_id, user3, user2);
+    // Transfer the NFT to user2
+    await nftMarketplace.transferNFT(nft2_id, user2);
 
     // Assertions
     const nft2_owner = await nftMarketplace.ownerOf(nft2_id);
-    assert.equal(nft2_owner, user3, "Transfer NFT failed");
+    assert.equal(nft2_owner, user2, "Transfer NFT failed");
   });
 
   // Test list NFT for sale
   it("Test list NFT for sale", async () => {
-    // Get account address
-    const user3 = accounts[3];
-
     // Declare NFT info variables
     const nft3_id = 3;
     const nft3_name = "NFT3";
@@ -65,10 +60,10 @@ contract('NFTMarketplace', (accounts) => {
     const nft3_price = 100;
 
     // Create NFT
-    await nftMarketplace.createNFT(nft3_id, nft3_name, nft3_description, user3);
+    await nftMarketplace.createNFT(nft3_id, nft3_name, nft3_description);
     
     // List the NFT for sale
-    await nftMarketplace.listNFTForSale(nft3_id, nft3_price, user3);
+    await nftMarketplace.listNFTForSale(nft3_id, nft3_price);
 
     // Assertions
     const nft3 = await nftMarketplace.getNFT(nft3_id);
@@ -78,9 +73,6 @@ contract('NFTMarketplace', (accounts) => {
 
   // Test remove NFT from sale
   it("Test remove NFT from sale", async () => {
-    // Get account address
-    const user4 = accounts[4];
-
     // Declare NFT info variables
     const nft4_id = 4;
     const nft4_name = "NFT4";
@@ -88,13 +80,13 @@ contract('NFTMarketplace', (accounts) => {
     const nft4_price = 100;
 
     // Create NFT
-    await nftMarketplace.createNFT(nft4_id, nft4_name, nft4_description, user4);
+    await nftMarketplace.createNFT(nft4_id, nft4_name, nft4_description);
 
     // List the NFT for sale
-    await nftMarketplace.listNFTForSale(nft4_id, nft4_price, user4);
+    await nftMarketplace.listNFTForSale(nft4_id, nft4_price);
 
     // Remove the NFT from sale
-    await nftMarketplace.removeNFTFromSale(nft4_id, user4);
+    await nftMarketplace.removeNFTFromSale(nft4_id);
 
     // Assertions
     const nft4 = await nftMarketplace.getNFT(nft4_id);
@@ -104,8 +96,8 @@ contract('NFTMarketplace', (accounts) => {
   // Test purchase NFT
   it("Test purchase NFT", async () => {
     // Get account address
-    const user5 = accounts[5];
-    const user6 = accounts[6];
+    const user5 = accounts[5]; // seller
+    const user6 = accounts[6]; // buyer
   
     // Declare NFT info variables
     const nft5_id = 5;
@@ -114,46 +106,46 @@ contract('NFTMarketplace', (accounts) => {
     const nft5_price = 100;
 
     // Create NFT
-    await nftMarketplace.createNFT(nft5_id, nft5_name, nft5_description, user5);
+    await nftMarketplace.createNFT(nft5_id, nft5_name, nft5_description, { from: user5 });
 
     // List the NFT for sale
-    await nftMarketplace.listNFTForSale(nft5_id, nft5_price, user5);
+    await nftMarketplace.listNFTForSale(nft5_id, nft5_price, { from: user5 });
   
     // Get original balances
-    const sellerOriginBalance = await web3.eth.getBalance(user5);
+    const seller_origin_balance = await web3.eth.getBalance(user5);
 
-    // Get commision Percentage
-    const commissionPercentage = await nftMarketplace.commissionPercentage();
+    // Get commision percentage
+    const commission_percentage = await nftMarketplace.commissionPercentage();
 
     // Get NFT price
     const nft5 = await nftMarketplace.getNFT(nft5_id);
     const price = nft5.price;
 
     // Calculate commision
-    const commission = price * commissionPercentage / 100;
+    const commission = price * commission_percentage / 100;
 
     // Make transaction
-    const salePriceBN = web3.utils.toBN(price);
-    const commissionBN = web3.utils.toBN(commission);
-    const totalCost = salePriceBN.add(commissionBN);
+    const sale_price_BN = web3.utils.toBN(price);
+    const commission_BN = web3.utils.toBN(commission);
+    const total_cost = sale_price_BN.add(commission_BN);
 
-    // Check buyer's balance
-    const buyerBalance = await web3.eth.getBalance(user6);
-    assert(web3.utils.toBN(buyerBalance).gte(totalCost), "Buyer's balance not enough");
+    // Check the NFT owner
+    assert.equal(nft5.owner, user5, "Incorrect owner");
 
     // purchase NFT
-    const tx = await nftMarketplace.purchaseNFT(nft5_id, { from: user6, value: totalCost.toString() });
+    const tx = await nftMarketplace.purchaseNFT(nft5_id, { from: user6, value: total_cost.toString() });
 
-    // Check NFT owner
-    const new_owner = await nftMarketplace.ownerOf(nft5_id);
-    assert.equal(new_owner, user6, "ownership not transferred to buyer");
+    // Check new NFT owner
+    const new_nft5 = await nftMarketplace.getNFT(nft5_id);
+    const new_nft5_owner = new_nft5.owner;
+    assert.equal(new_nft5_owner, user6, "Transfer NFT failed");
 
-    // Check seller's balance
+    // Get seller's balance after
     const seller_balance_after = await web3.eth.getBalance(user5);
 
     // Check seller's balance after purchasement
-    const fee = commissionBN;
-    const expected_seller_balance_after = web3.utils.toBN(sellerBalanceBefore).add(salePriceBN).sub(fee);
+    const fee = commission_BN;
+    const expected_seller_balance_after = web3.utils.toBN(seller_origin_balance).add(sale_price_BN).sub(fee);
     assert.equal(seller_balance_after, expected_seller_balance_after.toString(), "Incorrect balance of seller");
   });
 });
